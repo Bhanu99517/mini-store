6d6c79e45783bbd43bbeb812741fab85151a07c8
Bhanu🧑‍💻 <bhanu99517@gmail.com>
2026-06-05 13:41:18 +0530

    Update AppShowcase.tsx

--- src/components/AppShowcase.tsx
+++ src/components/AppShowcase.tsx
@@ -113,8 +113,8 @@ const ALL_APPS = [
     qrCodeUrl: 'https://rdnrbgocszptdiljdcvw.supabase.co/storage/v1/object/public/shared-files/7173b553-2c48-4f0a-9338-6c5ee1e3814d/8c2507d9-ebdf-43c0-abbf-83c2f66ac6d3.png',
     description: 'Share Drop is a secure peer-to-peer file sharing app that allows users to quickly and easily transfer files between devices without the need for cloud storage. With end-to-end encryption and a simple interface, Share Drop makes it easy to share photos, videos, documents, and more with friends and family.',
     slides: [
-      { id: 1, img: "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?w=500&auto=format&fit=crop&q=80" },
-      { id: 2, img: "https://images.unsplash.com/photo-1614850523459-c2f4c699c52e?w=500&auto=format&fit=crop&q=80" },
+      { id: 1, img: "https://rdnrbgocszptdiljdcvw.supabase.co/storage/v1/object/public/shared-files/7173b553-2c48-4f0a-9338-6c5ee1e3814d/be6bb19e-9bff-445e-97f0-1ff10d2a3ce9.jpg" },
+      { id: 2, img: "https://rdnrbgocszptdiljdcvw.supabase.co/storage/v1/object/public/shared-files/7173b553-2c48-4f0a-9338-6c5ee1e3814d/e8bc5211-3fc3-4fd8-b704-91c8e6efc73a.jpg" },
       { id: 3, img: "https://images.unsplash.com/photo-1626379616459-b2ce1d9decbc?w=500&auto=format&fit=crop&q=80" },
       { id: 4, img: "https://images.unsplash.com/photo-1634017839464-5c339ebe3cb4?w=500&auto=format&fit=crop&q=80" }
     ]
