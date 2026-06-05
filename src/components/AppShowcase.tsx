70ef0001a53a400f8d1e625e160f79a27213f9e7
Bhanu🧑‍💻 <bhanu99517@gmail.com>
2026-06-05 07:31:00 +0530

    logo

--- src/components/AppShowcase.tsx
+++ src/components/AppShowcase.tsx
@@ -106,7 +106,7 @@ const ALL_APPS = [
     name: 'Share Drop APK',
     publisher: 'bhanutechlab.',
     publisherUrl: 'https://bhanutechlab.vercel.app/projects',
-    logoUrl: "https://rdnrbgocszptdiljdcvw.supabase.co/storage/v1/object/public/shared-files/7173b553-2c48-4f0a-9338-6c5ee1e3814d/e5f97db3-aa68-45bf-b781-70b6ea48a9a3.jpg",
+    logoUrl: "https://rdnrbgocszptdiljdcvw.supabase.co/storage/v1/object/public/shared-files/7173b553-2c48-4f0a-9338-6c5ee1e3814d/24938353-1fd0-48de-be3d-f56b4cb25f5e.jpeg",
     downloads: '10+',
     size: '4.5 MB',
     apkUrl: 'https://rdnrbgocszptdiljdcvw.supabase.co/storage/v1/object/public/shared-files/7173b553-2c48-4f0a-9338-6c5ee1e3814d/07dffb63-b17e-40cc-b437-c14a22cf81a8.apk',
